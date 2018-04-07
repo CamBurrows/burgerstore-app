a9a51480cfbc860328d0c8b7c5c8aab9c5252841
Cameron Burrows <cburrows@Camerons-MacBook-Pro.local>
2018-04-07 00:47:37 -0400

    app progress

--- config/orm.js
+++ config/orm.js
@@ -15,7 +15,7 @@ function printQuestionMarks(num) {
 function objToSql(ob) {
     var arr = [];
   
-    // loop through the keys and push the key/value as a string int arr
+    // loop through the keys and push the key/value as a string into arr
     for (var key in ob) {
       var value = ob[key];
       // check to skip hidden properties
@@ -33,3 +33,59 @@ function objToSql(ob) {
     // translate array of strings to a single comma-separated string
     return arr.toString();
   }
+
+
+
+  var orm = {
+    //method for selecting all entries
+    all: function(tableInput, cb) {
+      var queryString = "SELECT * FROM " + tableInput + ";";
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+    //method for creating new entry
+    create: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+    
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+    
+        console.log(queryString);
+    
+        connection.query(queryString, vals, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      },
+      //method for updating entry
+    update: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+    
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+    
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      },
+
+}
+
